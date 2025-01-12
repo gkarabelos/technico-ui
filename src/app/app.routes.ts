31633166be66c5390054a0da704787a807cdcfc6
gkarabelos <gkarabelos@outlook.com>
2025-01-12 15:16:23 +0200

    Return to the Previous Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,17 @@ import { PropertiesComponent } from './pages/admin/properties/properties.compone
 import { RepairsComponent } from './pages/admin/repairs/repairs.component';
 import { CreatePropertyComponent } from './pages/admin/properties/components/create-property/create-property.component';
 import { UpdatePropertyComponent } from './pages/admin/properties/components/update-property/update-property.component';
-import { LoginComponent } from './pages/login/login.component';
-import { AuthGuard } from './shared/guards/auth.guard';
+
 
 export const routes: Routes = [
     {
-        path:'',
-        redirectTo: 'login',
-        pathMatch: 'full'
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
     },
     {
         path: 'home',
-        component: HomeComponent,
-        canActivate: [AuthGuard]
+        component: HomeComponent
     },
     {
         path: 'properties',
@@ -33,9 +31,5 @@ export const routes: Routes = [
     { 
         path: 'update-property/:id', 
         component: UpdatePropertyComponent 
-    },
-    {
-        path:'login',
-        component:LoginComponent
     }
 ];
